5bdbf8a904efbe60d70b4fa6abb52a47f0aebe4e
ajmalroshan1101 <ajumalroshan17@gmail.com>
2024-04-08 11:45:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo';
+
+  constructor(private http:HttpClient){}
+
+  backend!:string 
+
+  demofunction():Observable<any>{
+
+    return this.http.get<any>(`http://localhost:5000/common/demo`);
+  }
+
+  ngOnInit(): void {
+    
+    this.demofunction().subscribe({
+      next:(data)=>{
+        console.log(data);
+
+        this.backend= data
+        
+      },
+      error:(err)=>{
+        console.log(err);
+        
+      }
+    })
+  }
 }
+
+
